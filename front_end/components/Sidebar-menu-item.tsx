0e34dcb172354f503de8b1f7ff7c3a01b0ba4e18
Robert Matundu <matundukabamba@gmail.com>
2024-05-25 20:53:11 +0200

    first commit

--- front_end/components/Sidebar-menu-item.tsx
+++ front_end/components/Sidebar-menu-item.tsx
@@ -5,14 +5,20 @@ import React from 'react'
 const SidebarMenuItem = ({item}:{item:sideNavitem}) => {
   const LinkStyle =
     "flex items-center min-h-[40px] h-full text-slate-400 py-2 px-4 hover:text-white transition duration-300";
+    const ddLinkStyle = LinkStyle;
   return (
     <>
       {item.submenu ? (
-        <div className='rounded-md min-w-[18px]'></div>
+        <div className="rounded-md min-w-[18px]">
+          <a className={ddLinkStyle}>
+            {item.icon}
+            <span className="ml-3 leading-6 font-semibold">{item.title}</span>
+          </a>
+        </div>
       ) : (
         <Link href={item.path} className={LinkStyle}>
           {item.icon}
-          <span className='ml-3 leading-6 font-semibold'>{item.title}</span>
+          <span className="ml-3 leading-6 font-semibold">{item.title}</span>
         </Link>
       )}
     </>
