ebe836b926dd548fe02694e09496749bc9266410
Robert Matundu <matundukabamba@gmail.com>
2024-05-25 15:21:27 +0200

    first commit

--- front_end/components/Header.tsx
+++ front_end/components/Header.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { IoMenuSharp } from "react-icons/io5";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <>
+      <header className="fixed w-full bg-gray-800 shadow-sm shadow-slate-500/40 z-0 px-4 pl-[20rem]">
+        <div className="flex items-center justify-between h-16 px-3">
+          <button className="bg-slate-700 px-1 py-1 rounded-md shadow-md shadow-slate-600 text-slate-300 transition duration-300 ease-in-out hover:bg-slate-100 hover:text-slate-700 flex items-center justify-center">
+            <IoMenuSharp size={28} />
+          </button>
+          <div className="flex items-center justify-center h-10 w-10 rounded-full text-center bg-slate-700 text-slate-300">
+            <span className="font-semibold text-sm">RM</span>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header
--- front_end/components/PageWrapper.tsx
+++ front_end/components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const PageWrapper = ({children}:{children:ReactNode}) => {
   return (
-    <div>
+    <div className=' bg-slate-50 flex-grow p-2 mt-16 text-black'>
       {children}
     </div>
   )
