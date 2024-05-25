6e127173de980563f89e13c5dc15cc693b740e27
Robert Matundu <matundukabamba@gmail.com>
2024-05-25 20:03:59 +0200

    first commit

--- front_end/components/Sidebar-menu-item.tsx
+++ front_end/components/Sidebar-menu-item.tsx
@@ -5,13 +5,16 @@ import React from 'react'
 const SidebarMenuItem = ({item}:{item:sideNavitem}) => {
   return (
     <>
-      {
-        item.submenu
-          ?(<div></div>)
-          :(<Link href={item.path}>{item.title}</Link>)
-      }
+      {item.submenu ? (
+        <div></div>
+      ) : (
+        <Link href={item.path} className="flex space-x-2">
+          {item.icon}
+          <span>{item.title}</span>
+        </Link>
+      )}
     </>
-  )
+  );
 }
 
 export default SidebarMenuItem
