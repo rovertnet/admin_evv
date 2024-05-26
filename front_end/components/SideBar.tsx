cea53cea299ad167d05e9c3e3ef6f97c71948c0d
Robert Matundu <matundukabamba@gmail.com>
2024-05-26 16:21:44 +0200

    first commit

--- front_end/components/SideBar.tsx
+++ front_end/components/SideBar.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image';
 import { SIDENAV_ITEM } from '../SIDEBAR_CONSTANTS';
 import SidebarMenuItem from './Sidebar-menu-item';
 
-const SideBar = () => {
+const SideBar = ({toggleCollaps}:{toggleCollaps:boolean}) => {
+  const asideStyle = "fixed bg-gray-800 text-gray-400 z-50 h-full shadow-lg shadow-gray-900/20 transition duration-300 ease-in-out w-[20rem]"
   return (
     <>
-      <div className="fixed bg-gray-800 text-gray-400 z-50 h-full shadow-lg shadow-gray-900/20 transition duration-300 ease-in-out w-[20rem]">
+      <div className={asideStyle}>
         <div className="flex relative items-center py-8 px-3.5">
           <Image
             src="/mon_logo.png"
